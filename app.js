f686aa837186754e6de346989415596f9383f815
Ellen <ellen31725@gmail.com>
2023-02-10 23:06:41 +0800

    feat: add auth and related packages

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 require('./config/mongoose')
 
+const UsePassport = require('./config/passport')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers: {
-    ifEquals: function (value1, value2) {
-      return value1 === value2
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      ifEquals: function (value1, value2) {
+        return value1 === value2
+      }
     }
-  }
-}))
+  })
+)
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true // 未登入過的使用者，未初始化的session建立後強制塞進去
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+UsePassport(app)
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user// 反序列化時放入
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 // setting static files
 app.use(express.static('public'))
 
